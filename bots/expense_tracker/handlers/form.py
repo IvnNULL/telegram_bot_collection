ce717b1a8cdb12ad76783e0a544d46adb80dd2f9
Ivan <diesel0322@gmail.com>
2026-03-15 11:05:40 +0300

    feat(expense_bot): +form.py handler

---
+++ bots/expense_tracker/handlers/form.py
@@ -0,0 +1,77 @@
+import logging
+
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
+from states.states import FSMFillForm
+from texts.texts import TEXTS
+from keyboards.keyboards import get_save_form_kb
+from datetime import date
+
+logger = logging.getLogger(__name__)
+
+form_router = Router()
+
+
+@form_router.message(Command(commands='add'), StateFilter(default_state))
+async def process_add_command(message: Message, state: FSMContext):
+    await state.set_state(FSMFillForm.fill_category)
+    await state.update_data(date=date.today())
+    await message.answer(text=TEXTS['fill_category'])
+
+
+@form_router.message(StateFilter(FSMFillForm.fill_category), F.text)
+async def process_category_send(message: Message, state: FSMContext):
+    await state.update_data(category=message.text)
+    await state.set_state(FSMFillForm.fill_place)
+    await message.answer(text=TEXTS['fill_place'])
+
+
+@form_router.message(StateFilter(FSMFillForm.fill_place), F.text)
+async def process_place_send(message: Message, state: FSMContext):
+    await state.update_data(place=message.text)
+    await state.set_state(FSMFillForm.fill_description)
+    await message.answer(text=TEXTS['fill_description'])
+
+
+@form_router.message(StateFilter(FSMFillForm.fill_description), F.text)
+async def process_description_send(message: Message, state: FSMContext):
+    await state.update_data(description=message.text)
+    await state.set_state(FSMFillForm.fill_amount)
+    await message.answer(text=TEXTS['fill_amount'])
+
+
+@form_router.message(StateFilter(FSMFillForm.fill_amount), F.text)
+async def process_amount_send(message: Message, state: FSMContext):
+    await state.update_data(amount=message.text)
+    await state.set_state(FSMFillForm.fill_payer)
+    await message.answer(text=TEXTS['fill_payer'])
+
+
+@form_router.message(StateFilter(FSMFillForm.fill_payer), F.text)
+async def process_payer_send(message: Message, state: FSMContext):
+    await state.update_data(payer=message.text)
+    data = await state.get_data()
+
+    await message.answer(
+        text=f'Дата: {data['date']}\n'
+             f'Категория: {data['category']}\n'
+             f'Место: {data['place']}\n'
+             f'Описание: {data['description']}\n'
+             f'Сумма: {data['amount']}\n'
+             f'Плательщик: {data['payer']}',
+        reply_markup=get_save_form_kb()
+    )
+    await state.set_state(FSMFillForm.save_form)
+
+
+@form_router.callback_query(StateFilter(FSMFillForm.save_form), F.data.in_(['save_form', 'cancel_form']))
+async def process_save_form(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_reply_markup()
+    if callback.data == 'save_form':
+        await callback.message.answer(text=TEXTS['save_form'])
+    else:
+        await callback.message.edit_text(text=TEXTS['cancel_form'])
+    await state.clear()
