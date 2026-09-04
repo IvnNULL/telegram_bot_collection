112b3e737f08e8e479bd94d723c8c133b91d98f9
Ivan <diesel0322@gmail.com>
2026-09-04 13:56:48 +0300

    feat(expense_bot): add expense list by date

--- bots/expense_tracker_bot/database/repositories/expense.py
+++ bots/expense_tracker_bot/database/repositories/expense.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import date
 
-from sqlalchemy import select, delete
+from sqlalchemy import select, delete, func
 
 from database.models import Expense
 from database.repositories.base import BaseRepository
@@ -11,15 +11,15 @@ logger = logging.getLogger(__name__)
 
 class ExpenseRepository(BaseRepository):
     async def add_expense(
-            self,
-            *,
-            user_id: int,
-            exp_date: date,
-            category: str,
-            place: str,
-            description: str,
-            amount: int,
-            payer: str
+        self,
+        *,
+        user_id: int,
+        exp_date: date,
+        category: str,
+        place: str,
+        description: str,
+        amount: int,
+        payer: str,
     ) -> None:
         expense = Expense(
             user_id=user_id,
@@ -28,7 +28,7 @@ class ExpenseRepository(BaseRepository):
             place=place,
             description=description,
             amount=amount,
-            payer=payer
+            payer=payer,
         )
         self.session.add(expense)
         logger.debug(f'Add expense from {user_id}')
@@ -36,11 +36,20 @@ class ExpenseRepository(BaseRepository):
         logger.debug(f'Add expense from {user_id} completed')
 
     async def list_expenses(self) -> list[Expense] | None:
-        data = await self.session.scalars(
-            select(Expense).order_by(Expense.date)
-        )
+        data = await self.session.scalars(select(Expense).order_by(Expense.date))
         expenses = data.all()
         return expenses if expenses else None
+
+    async def get_latest_expense_date(self) -> date | None:
+        latest_date = await self.session.scalar(select(func.max(Expense.date)))
+        return latest_date
+
+    async def get_expenses_by_date(self, target_date: date) -> list[Expense]:
+        data = await self.session.scalars(
+            select(Expense).where(Expense.date == target_date).order_by(Expense.id.desc())
+        )
+        expenses = data.all()
+        return expenses
 
     async def clear_expenses(self) -> None:
         await self.session.execute(delete(Expense))
--- bots/expense_tracker_bot/handlers/expense_edit.py
+++ bots/expense_tracker_bot/handlers/expense_edit.py
@@ -1,11 +1,16 @@
 import logging
+from datetime import date
 
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from database.repositories import ExpenseRepository
+from keyboards.keyboards import get_expenses_keyboard
+from services.expense_service import prepare_edit_data
+from sqlalchemy.ext.asyncio import AsyncSession
 from states.states import FSMEditExpense
 from texts.texts import TEXTS
 
@@ -15,11 +20,27 @@ edit_router = Router()
 
 
 @edit_router.message(StateFilter(default_state), Command(commands='edit'))
-async def process_edit_command(message: Message, state: FSMContext):
+async def process_edit_command(message: Message, state: FSMContext, session: AsyncSession):
     await message.delete()
     await state.set_state(FSMEditExpense.browsing)
-    edit_message = await message.answer(text=TEXTS['edit_start'])
-    await state.update_data(edit_message_id=edit_message.message_id)
+
+    expense_repo = ExpenseRepository(session)
+    latest_date = await expense_repo.get_latest_expense_date()
+    if latest_date is None:
+        latest_date = date.today()
+    expenses = await expense_repo.get_expenses_by_date(latest_date)
+    data = prepare_edit_data(expenses)
+
+    edit_message = await message.answer(
+        text=TEXTS['edit_start'],
+        reply_markup=get_expenses_keyboard(
+            data['expenses_buttons'][data['current_page'] - 1],
+            data['current_page'],
+            data['total_pages'],
+            latest_date,
+        ),
+    )
+    await state.update_data(edit_message_id=edit_message.message_id, date=latest_date.isoformat(), **data)
 
 
 @edit_router.message(StateFilter(FSMEditExpense), Command('cancel'))
@@ -31,3 +52,19 @@ async def process_cancel_command(message: Message, state: FSMContext):
     except TelegramBadRequest as e:
         logger.error(e)
     await state.clear()
+
+
+@edit_router.callback_query(StateFilter(FSMEditExpense), F.data == 'cancel')
+async def process_cancel_click(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+
+
+@edit_router.callback_query(StateFilter(FSMEditExpense))
+async def process_click(callback: CallbackQuery):
+    await callback.answer()
+
+
+@edit_router.message(StateFilter(FSMEditExpense))
+async def process_wrong_send(message: Message):
+    await message.delete()
