bf23acd80aed26b3e6e48575f33feb558866b249
Ivan <diesel0322@gmail.com>
2026-09-24 17:30:37 +0300

    refactor(expense_bot): separate welcome message from main menu flow

--- bots/expense_tracker_bot/handlers/user.py
+++ bots/expense_tracker_bot/handlers/user.py
@@ -2,11 +2,12 @@ import logging
 
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.repositories import ExpenseRepository
-from handlers.main_menu import send_main_menu
+from keyboards.keyboards import get_show_menu_kb
 from services.expense_service import expenses_to_text, expenses_to_csv_text, text_to_csv_file
 from texts.texts import TEXTS
 
@@ -16,9 +17,10 @@ user_router = Router()
 
 
 @user_router.message(CommandStart())
-async def process_start_command(message: Message):
-    # await message.answer(text=TEXTS['/start'])
-    await send_main_menu(message)
+async def process_start_command(message: Message, state: FSMContext):
+    await message.delete()
+    await state.clear()
+    await message.answer(text=TEXTS['/start'], reply_markup=get_show_menu_kb())
 
 
 @user_router.message(Command(commands='help'))
